f324a94f9368d613fd7eae9bde54a148950e2c22
Laura Roque <lroquecu@gmail.com>
2024-12-11 16:20:26 +0100

    vpc

---
+++ networking.tf
@@ -0,0 +1,20 @@
+locals {
+  public_subnets_cidr  = [for k, v in var.subnet_config : v.cidr_block if v.public]
+}
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.3"
+
+  name                    = "${local.project_name}-vpc"
+  cidr                    = var.vpc_cidr
+  azs                     = data.aws_availability_zones.azs.names
+  public_subnets          = local.public_subnets_cidr
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    Name = "${local.project_name}-vpc"
+  })
+}
