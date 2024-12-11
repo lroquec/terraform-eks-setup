763ec3ec1150dc808ed294ee72406f8e0f35acd6
Laura Roque <lroquecu@gmail.com>
2024-12-11 16:54:39 +0100

    add sg

--- compute.tf
+++ compute.tf
@@ -116,11 +116,16 @@ resource "aws_eks_node_group" "worker-node-group" {
   node_role_arn   = aws_iam_role.workernodes.arn
   subnet_ids      = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
   instance_types = var.instanceType
-
+  disk_size      = 20
   scaling_config {
     desired_size = var.desired_size
     max_size     = var.max_size
     min_size     = var.min_size
+  }
+
+  remote_access {
+    ec2_ssh_key = var.ec2_ssh_key
+    source_security_group_ids = [aws_security_group.eks_nodes_sg.id]
   }
 
   depends_on = [
@@ -132,4 +137,83 @@ resource "aws_eks_node_group" "worker-node-group" {
 resource "aws_eks_addon" "csi" {
   cluster_name = aws_eks_cluster.eks.name
   addon_name   = "aws-ebs-csi-driver"
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "9e99a48a9960a6e3c123f6cf3f97cd3a17da5c85"
+  ]
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+
+# Security Group for EKS cluster
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg"
+  description = "EKS Cluster Security Group"
+  vpc_id      = module.vpc.vpc_id
+
+  # Allow inbound traffic on port 443
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.tester-ip] # Tester public IP
+  }
+
+  # Egress rule for all traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "eks-cluster-sg"
+  }
+}
+
+# Security Group for EKS nodes
+resource "aws_security_group" "eks_nodes_sg" {
+  name        = "eks-nodes-sg"
+  description = "EKS Nodes Security Group"
+  vpc_id      = module.vpc.vpc_id
+
+  # Allow inbound traffic on port 22 from the tester IP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.tester-ip] # Tester public IP
+  
+  }
+
+  # Allow communication between nodes
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    security_groups = [aws_security_group.eks_nodes_sg.id]
+  }
+
+  # Allow connection with the EKS cluster
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.eks_cluster_sg.id]
+  }
+
+  # Egress rule for all traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "eks-nodes-sg"
+  }
 }
