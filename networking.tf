649a0b89eb782d5d289834b83236d750c42df010
Laura Roque <lroquecu@gmail.com>
2024-12-15 09:52:10 +0100

    add private subnets

--- networking.tf
+++ networking.tf
@@ -1,5 +1,6 @@
 locals {
   public_subnets_cidr = [for k, v in var.subnet_config : v.cidr_block if v.public]
+  private_subnets_cidr = [for k, v in var.subnet_config : v.cidr_block if !v.public]
 }
 data "aws_availability_zones" "azs" {
   state = "available"
@@ -11,6 +12,7 @@ module "vpc" {
   name                    = "${local.project_name}-vpc"
   cidr                    = var.vpc_cidr
   azs                     = data.aws_availability_zones.azs.names
+  private_subnets         = local.private_subnets_cidr
   public_subnets          = local.public_subnets_cidr
   map_public_ip_on_launch = true
 
