b71298debc941282c331cef80fe95d161387e789
Laura Roque <lroquecu@gmail.com>
2024-12-15 16:35:27 +0100

    database

--- networking.tf
+++ networking.tf
@@ -18,7 +18,33 @@ module "vpc" {
   enable_nat_gateway      = true
   single_nat_gateway      = true
 
+    # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # Database Subnets
+  database_subnets = var.vpc_database_subnets
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+
   tags = merge(local.common_tags, {
     Name = "${local.project_name}-vpc"
   })
+
+  # Additional Tags to Subnets
+  public_subnet_tags = {
+    Type = "public-subnets"
+    "kubernetes.io/role/elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"        
+  }
+  private_subnet_tags = {
+    Type = "private-subnets"
+    "kubernetes.io/role/internal-elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
 }
