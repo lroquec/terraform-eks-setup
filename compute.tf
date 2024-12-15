223214268e6bbff897c7f225c50e8f7a8ffbed1b
Laura Roque <lroquecu@gmail.com>
2024-12-15 09:53:17 +0100

    i dont need ecr

--- compute.tf
+++ compute.tf
@@ -21,10 +21,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks-iam-role.name
 }
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-iam-role.name
-}
 
 ## Create the EKS cluster
 resource "aws_eks_cluster" "eks" {
