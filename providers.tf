e565e4e102bd465f8a29f9641aaad9b45340472f
Laura Roque <lroquecu@gmail.com>
2024-12-15 10:44:29 +0100

    update provider version

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.76.0"
+      version = "~> 5.81.0"
     }
     random = {
       source  = "hashicorp/random"
