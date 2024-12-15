48395e545f86248edcc9de54e0317eb46af5edb8
Laura Roque <lroquecu@gmail.com>
2024-12-15 18:22:21 +0100

    add kubernetes provider

--- providers.tf
+++ providers.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.9.0"
