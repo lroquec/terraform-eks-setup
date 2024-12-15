2067f49cf119965d71a6081a3c96e3a5ab5adb24
Laura Roque <lroquecu@gmail.com>
2024-12-15 18:28:53 +0100

    correction

--- providers.tf
+++ providers.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.35.0"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.9.0"
