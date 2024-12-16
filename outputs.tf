27f957ce137cc4685b19f288421c5a3210005b91
Laura Roque <lroquecu@gmail.com>
2024-12-16 09:35:35 +0100

    add oidc outputs

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,20 @@ output "cluster_certificate_authority_data" {
   description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
   value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
+
+# Output: AWS IAM Open ID Connect Provider ARN
+output "aws_iam_openid_connect_provider_arn" {
+  description = "AWS IAM Open ID Connect Provider ARN"
+  value = aws_iam_openid_connect_provider.oidc_provider.arn 
+}
+
+# Extract OIDC Provider from OIDC Provider ARN
+locals {
+    aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
+}
+
+# Output: AWS IAM Open ID Connect Provider
+output "aws_iam_openid_connect_provider_extract_from_arn" {
+  description = "AWS IAM Open ID Connect Provider extract from ARN"
+   value = local.aws_iam_oidc_connect_provider_extract_from_arn
+}
