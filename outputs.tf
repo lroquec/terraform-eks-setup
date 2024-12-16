ae04192544899d796199e074a072649cc8c2f0f6
Laura Roque <lroquecu@gmail.com>
2024-12-16 16:38:29 +0100

    correct outputs

--- outputs.tf
+++ outputs.tf
@@ -23,11 +23,11 @@ output "cluster_certificate_authority_data" {
 # Output: AWS IAM Open ID Connect Provider ARN
 output "aws_iam_openid_connect_provider_arn" {
   description = "AWS IAM Open ID Connect Provider ARN"
-  value       = aws_iam_openid_connect_provider.eks_oidc_provider.arn
+  value       = module.eks.oidc_provider_arn
 }
 
 # Output: AWS IAM Open ID Connect Provider
 output "aws_iam_openid_connect_provider_extract_from_arn" {
   description = "AWS IAM Open ID Connect Provider extract from ARN"
-  value       = local.aws_iam_oidc_connect_provider_extract_from_arn
+  value       = module.eks.oidc_provider
 }
