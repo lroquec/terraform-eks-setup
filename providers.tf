04a714fd148f7f2fb20a03a6b4c6be5ec78b8780
Laura Roque <lroquecu@gmail.com>
2024-12-16 14:43:45 +0100

    update providers

--- providers.tf
+++ providers.tf
@@ -16,6 +16,14 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.14.0"
+    }
   }
 }
 
