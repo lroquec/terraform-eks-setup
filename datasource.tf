16d1fdf08c82c4eea8b2b364439a46b14bdf1d86
Laura Roque <lroquecu@gmail.com>
2024-12-16 09:59:47 +0100

    not needed here

--- datasource.tf
+++
@@ -1,8 +0,0 @@
-# data "terraform_remote_state" "eks" {
-#   backend = "s3"
-#   config = {
-#     bucket = "lroquec-tf"
-#     key    = "eks/eks-terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
