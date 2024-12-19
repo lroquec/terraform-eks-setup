573608007c8451d15a9def33c11c1234e9288fa9
Laura Roque <lroquecu@gmail.com>
2024-12-19 07:55:02 +0100

    use patch

--- 01-vpc-and-cluster-setup/usersmagmt.tf
+++ 01-vpc-and-cluster-setup/usersmagmt.tf
@@ -1,3 +1,10 @@
+module "eks-auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.0"
+
+  manage_aws_auth_configmap = false
+}
+
 # Get AWS Account ID
 data "aws_caller_identity" "current" {}
 output "account_id" {
@@ -6,12 +13,15 @@ output "account_id" {
 
 # Locals Block
 locals {
-  configmap_roles = [
-    {
-      rolearn  = "${module.eks.node_iam_role_arn}"
-      username = "system:node:{{EC2PrivateDNSName}}"
-      groups   = ["system:bootstrappers", "system:nodes"]
-    },
+  
+  first_node_group_key = keys(module.eks.eks_managed_node_groups)[0]
+  aws_auth_configmap_data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = module.eks.eks_managed_node_groups[local.first_node_group_key].iam_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      },
     {
       rolearn  = "${aws_iam_role.eks_admin_role.arn}"
       username = "eks-admin" # Just a place holder name
@@ -30,22 +40,37 @@ locals {
       #groups   = [ "eks-developer-group" ]
       # Important Note: The group name specified in clusterrolebinding and in aws-auth configmap groups should be same.       
       groups = ["${kubernetes_role_binding_v1.eksdeveloper_rolebinding.subject[0].name}"]
-    },
-  ]
-}
-# Resource: Kubernetes Config Map
-resource "kubernetes_config_map_v1" "aws_auth" {
-  depends_on = [
-    module.eks,
-    kubernetes_cluster_role_binding_v1.eksreadonly_clusterrolebinding,
-    kubernetes_cluster_role_binding_v1.eksdeveloper_clusterrolebinding,
-    kubernetes_role_binding_v1.eksdeveloper_rolebinding
-  ]
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
+    }
+    ] )
+    
+    mapUsers = yamlencode([
+      {
+      userarn  = "${aws_iam_user.eksadmin_user.arn}"
+      username = "${aws_iam_user.eksadmin_user.name}"
+      groups   = ["system:masters"]
+      }
+    ] )
   }
-  data = {
-    mapRoles = yamlencode(local.configmap_roles)
+    patch_data = jsonencode({
+    data = {
+      mapRoles = local.aws_auth_configmap_data.mapRoles
+      mapUsers = local.aws_auth_configmap_data.mapUsers
+    }
+  })
+}
+
+# Resource to update: Kubernetes Config Map
+resource "null_resource" "update_aws_auth" {
+  depends_on = [module.eks]
+
+  triggers = {
+    auth_map = local.patch_data
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws eks update-kubeconfig --name ${module.eks.cluster_name} --region us-east-1
+      kubectl patch configmap/aws-auth -n kube-system --patch '${local.patch_data}'
+    EOT
   }
 }
